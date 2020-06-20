7a040e8c87c64ec71e2e89c8de780b9d5bd5eae7
kean <grebenyuk.alexander@gmail.com>
2020-06-20 15:56:50 -0400

    Add some basic performance tests

---
+++ Tests/AnchorPerformanceTests.swift
@@ -0,0 +1,35 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2017-2020 Alexander Grebenyuk (github.com/kean).
+
+import XCTest
+import Align
+
+// WARNING: Don't forget to compile for Release mode!
+final class AnchorPerformanceTests: XCTestCase {
+    func testPin() {
+        let view = View()
+        let container = View()
+        container.addSubview(view)
+
+        measure {
+            for _ in 0...1000 {
+                view.anchors.edges.pin(alignment: .center)
+            }
+        }
+    }
+
+    func testConstraintsPin() {
+        let view = View()
+        let container = View()
+        container.addSubview(view)
+
+        measure {
+            Constraints {
+                for _ in 0...10000 {
+                    view.anchors.width.equal(view.anchors.height)
+                }
+            }
+        }
+    }
+}
